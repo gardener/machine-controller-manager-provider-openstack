ac39266e5c44e1df241ad538b7ae633e3639a993
Rishabh Patel <66425093+rishabh-11@users.noreply.github.com>
2023-05-23 19:16:23 +0530

    Increase timeout for it to 10 min (#91)

--- test/integration/controller/controller_test.go
+++ test/integration/controller/controller_test.go
@@ -39,7 +39,8 @@ import (
 	"github.com/gardener/machine-controller-manager-provider-openstack/test/integration/provider"
 )
 
-var commons = common.NewIntegrationTestFramework(&provider.ResourcesTrackerImpl{}, 400)
+// the timeout is changed to accommodate for time taken by node-critical components to get ready. PR - https://github.com/gardener/machine-controller-manager/pull/778
+var commons = common.NewIntegrationTestFramework(&provider.ResourcesTrackerImpl{}, 600)
 
 var _ = BeforeSuite(commons.SetupBeforeSuite)
 
