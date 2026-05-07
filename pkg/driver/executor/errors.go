9ffe7e90436fc947eed743c1a28c85fccfc86fc1
viragvoros <virag.voeroes@inovex.de>
2026-05-07 16:28:17 +0200

    Feature: Map flavor not found to ResourceExhausted grpc code

--- pkg/driver/executor/errors.go
+++ pkg/driver/executor/errors.go
@@ -8,12 +8,17 @@ import (
 	"fmt"
 )
 
-// NoValidHost is a part of the error message returned when there is no valid host in the zone to deploy a VM.
-// Matches:
-//
-//	"No valid host was found."
-//	"No valid host was found. There are not enough hosts available."
-const NoValidHost = "No valid host was found"
+const (
+	// NoValidHost is a part of the error message returned when there is no valid host in the zone to deploy a VM.
+	// Matches:
+	//
+	//	"No valid host was found."
+	//	"No valid host was found. There are not enough hosts available."
+	NoValidHost = "No valid host was found"
+
+	// FlavorNotFound is part of the error message returned when a flavor cannot be resolved.
+	FlavorNotFound = "error resolving flavor"
+)
 
 var (
 	// ErrNotFound is returned when the requested resource could not be found.
